d54351497dd224ec5d37a303660f105554940ef6
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-10 18:26:32 +0100

    #12

--- sw.js
+++ sw.js
@@ -2,7 +2,11 @@ self.addEventListener("install", (event) => {
   console.log("service worker installed");
 });
 
-//
 self.addEventListener("activate", (event) => {
   console.log("service worker activated");
+});
+
+//
+self.addEventListener("fetch", (event) => {
+  console.log("fetch event", event);
 });
