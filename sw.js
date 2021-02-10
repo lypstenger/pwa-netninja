33078e7d6217fed3dbc6e853f6ad5fc98b9b66bf
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-10 19:10:54 +0100

    #15

--- sw.js
+++ sw.js
@@ -1,12 +1,32 @@
+const staticCacheName = "site-static";
+const assets = [
+  "/",
+  "/index.html",
+  "/js/app.js",
+  "/js/ui.js",
+  "/js/materialize.min.js",
+  "/css/styles.css",
+  "/css/materialize.min.css",
+  "/img/dish.png",
+  "https://fonts.googleapis.com/icon?family=Material+Icons",
+  //   "https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2",
+];
+
 self.addEventListener("install", (event) => {
-  console.log("service worker installed");
+  //   console.log("service worker installed");
+  event.waitUntil(
+    caches.open(staticCacheName).then((cache) => {
+      console.log("caching shell assets");
+      cache.addAll(assets);
+    })
+  );
 });
 
 self.addEventListener("activate", (event) => {
-  console.log("service worker activated");
+  //   console.log("service worker activated");
 });
 
 //
 self.addEventListener("fetch", (event) => {
-  console.log("fetch event", event);
+  //   console.log("fetch event", event);
 });
