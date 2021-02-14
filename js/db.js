d491528449da8aa8c2c99e4dbd59eb0fb51b171f
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-14 19:31:35 +0100

    #27

--- js/db.js
+++ js/db.js
@@ -24,3 +24,20 @@ db.collection("recipes").onSnapshot((snapshot) => {
     }
   });
 });
+
+// add new recipe
+const form = document.querySelector("form");
+form.addEventListener("submit", (evt) => {
+  evt.preventDefault();
+  const recipe = {
+    title: form.title.value,
+    ingredients: form.ingredients.value,
+  };
+
+  db.collection("recipes")
+    .add(recipe)
+    .catch((err) => console.log(err));
+
+  form.title.value = "";
+  form.ingredients.value = "";
+});
