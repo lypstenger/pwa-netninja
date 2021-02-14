6b9eb19e5c00b4df065fb7ae920908ddf643d02c
Jon Erik Ullvang <jon.erik.ullvang@gmail.com>
2021-02-14 18:58:00 +0100

    #25

--- js/db.js
+++ js/db.js
@@ -5,6 +5,7 @@ db.collection("recipes").onSnapshot((snapshot) => {
     console.log(change.d_, change.d_.doc.data(), change.d_.doc.id);
     if (change.d_.type === "added") {
       // add the document to the web page
+      renderRecipe(change.d_.doc.data(), change.d_.doc.id);
     }
     if (change.d_.type === "removed") {
     }
